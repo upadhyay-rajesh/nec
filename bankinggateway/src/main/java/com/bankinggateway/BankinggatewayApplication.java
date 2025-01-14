686027bd45903d7313302b4f4be67c493645b563
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2025-01-14 13:17:08 +0530

    ds

---
+++ bankinggateway/src/main/java/com/bankinggateway/BankinggatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bankinggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankinggatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankinggatewayApplication.class, args);
+	}
+
+}
---
+++ bankinggateway/src/test/java/com/bankinggateway/BankinggatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankinggatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
